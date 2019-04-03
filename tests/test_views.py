cc96f386d5b6d155df925f964d024e3e4866de49
Sivapregassam Selvain <selvain.sivapregassam@gmail.com>
2019-04-03 15:35:14 +0200

    exercice flask fini

--- tests/test_views.py
+++ tests/test_views.py
@@ -2,6 +2,15 @@
 from flask_testing import TestCase
 from wsgi import app
 
+class Counter:
+    def __init__(self):
+        self.id = 4
+
+    def next(self):
+        self.id += 1
+        return self.id
+
+
 class TestViews(TestCase):
     def create_app(self):
         app.config['TESTING'] = True
@@ -12,3 +21,36 @@ class TestViews(TestCase):
         products = response.json
         self.assertIsInstance(products, list)
         self.assertGreater(len(products), 2) # 2 is not a mistake here.
+
+    def test_read_single_found(self):
+        response = self.client.get("/api/v1/products/2")
+        product = response.json
+        result = { 'id': 2, 'name': 'Socialive.tv' }
+        self.assertEqual(product, result)
+        self.assertStatus(response,200)
+
+    def test_read_single_notfound(self):
+        response = self.client.get("/api/v1/products/7")
+        self.assertStatus(response,404)
+
+    def test_delete_existing_element(self):
+        response = self.client.delete("/api/v1/products/1")
+        self.assertStatus(response,204)
+
+    def test_delete_notexisiting_element(self):
+        response = self.client.delete("/api/v1/products/8")
+        self.assertStatus(response,404)
+
+    def test_create_valid_element(self):
+        ID = Counter()
+        response = self.client.post("/api/v1/products", json={ 'id':ID.next(), 'name':'newelement'})
+        self.assertStatus(response,201)
+
+    def test_update_valid_element(self):
+        response = self.client.patch("/api/v1/products/2", json={'name':'newname'})
+        self.assertStatus(response,204)
+
+    def test_update_nonvalid_element(self):
+        response = self.client.patch("/api/v1/products/8", json={'name':'newname'})
+        self.assertStatus(response,422)
+
--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,21 @@
 # wsgi.py
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort, request
+import json
 
 PRODUCTS = [
-    {'id': 1, 'name': 'Skello' },
+    { 'id': 1, 'name': 'Skello' },
     { 'id': 2, 'name': 'Socialive.tv' },
-    { 'id': 3, 'name': 'Coucou' }
+    { 'id': 3, 'name': 'Coucou' },
+    { 'id': 4, 'name': 'lenumeroquatre'}
 ]
 
 app = Flask(__name__)
+
+def find_product(id):
+    for product in PRODUCTS:
+        if product['id'] == id:
+            return product
+    return None
 
 @app.route('/')
 def hello():
@@ -16,3 +24,38 @@ def hello():
 @app.route('/api/v1/products')
 def show_products():
     return jsonify(PRODUCTS)
+
+@app.route('/api/v1/products/<int:id>', methods=['GET'])
+def show_product(id):
+    product = find_product(id)
+    if product == None:
+        abort(404)
+        return None
+    else:
+        product = jsonify(product)
+        return product
+
+@app.route('/api/v1/products/<int:id>', methods=['DELETE'])
+def delete_product(id):
+    product = find_product(id)
+    if product == None:
+        abort(404)
+    else:
+        PRODUCTS.remove(product)
+        return '',204
+
+@app.route('/api/v1/products', methods=['POST'])
+def create_products():
+    json = request.get_json()
+    PRODUCTS.append(json)
+    return '',201
+
+@app.route('/api/v1/products/<int:id>', methods=['PATCH'])
+def update_product(id):
+    product = find_product(id)
+    if product == None:
+        abort(422)
+    else:
+        jsonupdate = request.get_json()
+        product['name'] = jsonupdate['name']
+        return '',204
