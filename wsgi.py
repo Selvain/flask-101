02ccdd895a695ad23bec25c6d578da05fee2a1e3
Sivapregassam Selvain <selvain.sivapregassam@gmail.com>
2019-04-03 11:39:09 +0200

    new fonction in wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,17 @@
 # wsgi.py
-from flask import Flask
+from flask import Flask, jsonify
+
+PRODUCTS = [
+    {'id': 1, 'name': 'Skello' },
+    { 'id': 2, 'name': 'Socialive.tv' }
+]
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Welcome to the World!"
+
+@app.route('/api/v1/products')
+def show_products():
+    return jsonify(PRODUCTS)
